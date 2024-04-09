6d77dbd12d792dd0d4a2075afa16a406a341b37e
Michael Demidenko <demidenm@umich.edu>
2024-04-09 18:08:15 -0500

    Modified MakeFolders

--- scripts/misc/subj-ids_eprimetotsv.sh
+++ scripts/misc/subj-ids_eprimetotsv.sh
@@ -5,28 +5,35 @@ conda activate fmri_env
 
 # Script Description:
 # This Bash script is designed to automate the execution of a Python script for processing behavioral data.
-# It prompts the user to select a session, task and ERI file path, creates output directories, and
-# runs the Python script for each subject listed in a text file.
+# It prompts the user to select a session, task and subject IDs that have ERI in source, creates output directories, and 
+# runs the Python script for each subject listed in a text file. 
 # The script logs any errors that occur during the execution and provides a summary.
-# Note, this script works with only the direct paths to the EventRelated files for specified task/subject
+# Note, this script checks for subjects on NGDR and then S3 buckets
 
 # Usage:
 # Run this script from the command line with necessary permissions. It assumes the presence of a Python module,
-# a directory structure, and input data. The file should contain the full path to the ERI .txt with e-prime info
+# a directory structure, and input data.
 
 # Description of Steps:
 # 1. Set up directory paths and current date.
-# 2. Prompt user for session, task, and list of paths to subjects' ERI files.
+# 2. Prompt user for session, task, and list of subject IDs that have sourcedata ERI files on NGDR or S3 bucket.
 # 3. Create output directories based on user input.
 # 4. Run Python script for each subject path in the provided list.
 # 5. Log errors and completion status.
 # 6. Provide summary of script execution.
 
+# Script Author: Michael Demidenko
+# Date: March 2024
+
+
 dir=`pwd`
-tmp=${dir}/tmp
-files=${dir}/files
-log_out=${dir}/logs
+tmp=${dir}/../tmp
+files=${dir}/../files
+log_out=${dir}/../logs
 curr_date=$(date +"%m/%d/%Y")
+ngdr=/spaces/ngdr/ref-data/abcd/nda-3165-2020-09
+year1_s3=s3://ABCC_year1
+year2_s3=s3://ABCC_year2
 
 echo "Starting script..."
 
@@ -34,16 +41,16 @@ echo -e "Which session? Options: 2YearFollowUpYArm1, baselineYear1Arm1."
 read ses
 echo -e "Which task? Options: MID, SST, nback." 
 read task
-echo -e "Which subject ID list? Provide file with list of full path \n to *EventsInfo.txt"
+echo -e "Which subject ID list? Provide file with each row for ID w/o 'sub-' prefix"
 read subs
 
 echo "Options selected: ses: $ses, task: $task, sub: $subs ."
 
-echo -e "Which you like to proced with options: \n\tses-${ses} & task-${task}? y or n" 
+echo -e "Would you like to proced with options: \n\tses-${ses} & task-${task}? y or n" 
 read response
 
 # create output fold based on prompts
-out=${dir}/${ses}_${task}
+out=${dir}/../${ses}_${task}
 err_log=${log_out}/${ses}_${task}.err
 out_log=${log_out}/${ses}_${task}.out
 
@@ -71,19 +78,25 @@ fi
 
 # run python script
 c=0
-cat ${subs} | while read line ; do 
-	dir=$(dirname $line )
-	file=$(basename $line )
-	sub=$(echo $file | awk -F'_' '{ print $1}' | awk -F'-' '{ print $2}') 
-	ses=$(echo $file | awk -F'_' '{ print $2}' | awk -F'-' '{ print $2}')
-	run=$(echo $file | awk -F'_' '{ print $4}' | awk -F'-' '{ print $2}')
+cat ${subs} | while read sub ; do 
+	run=01
+	file_name=sub-${sub}_ses-${ses}_task-${task}_run-${run}_bold_EventRelatedInformation.txt
+	if [ -f ${ngdr}/sourcedata/sub-${sub}/ses-${ses}/func/${file_name} ] ; then
+		cp ${ngdr}/sourcedata/sub-${sub}/ses-${ses}/func/${file_name} ${tmp}/${file_name}
+	else
+		if [ "${ses}" == "baselineYear1Arm1" ] ;  then
+			s3cmd sync ${year1_s3}/sourcedata/sub-${sub}/ses-${ses}/func/${file_name} ${tmp}/${file_name}
+		elif [ "${ses}" == "2YearFollowUpYArm1" ] ; then
+			s3cmd sync ${year2_s3}/sourcedata/sub-${sub}/ses-${ses}/func/${file_name} ${tmp}/${file_name}
+		fi
+	fi
+
 	c=$((c+1))
 
 	echo "     Starting $sub [${c}]"
 	
-	cp $line $tmp
 	# run python script and save error for troubleshooting 
-	error_msg=$(python ../eprimetotsv.py -i ${tmp} -o ${out} -s ${sub} -e ${ses} -r ${run} -t ${task} 2>&1 | tr -d '\n')
+	error_msg=$(python ./abcc_eprimetotsv.py -i ${tmp} -o ${out} -s ${sub} -e ${ses} -r ${run} -t ${task} 2>&1 | tr -d '\n')
     	if [[ ! -z "${error_msg}" ]]; then
         	echo -e "${sub}\t${ses}\t${task}\t${run}\t${curr_date}\t${error_msg}" >> ${err_log}
     	else
@@ -92,7 +105,7 @@ cat ${subs} | while read line ; do
 		echo -e "${sub}\t${ses}\t${task}\t${run}\t${curr_date}\t${comp_msg}" >> ${out_log}
 	fi
 	
-	rm ${tmp}/${file}
+	rm ${tmp}/${file_name}
 
 done
 
